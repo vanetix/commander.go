1061ae382f5b75e0f7b9e5d1610b16d44439caee
vanetix <matmcfarland@gmail.com>
2013-01-29 08:32:37 -0600

    Add a simple test

--- commander_test.go
+++ commander_test.go
@@ -1,1 +1,23 @@
-package commander.go
+package commander
+
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	commander := Commander{
+		Name: "commander",
+		Version: "1.0.0",
+	}
+
+	commander.Add(Option{
+		Name: "Help",
+		Tiny: 'h',
+		Verbose: "help",
+		Description: "Display usage",
+		Callback: commander.Usage,
+		Required: true,
+	})
+
+	commander.Parse()
+}
