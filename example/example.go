9e4bb204466665e1b26d9231ff42d3a0814c81b6
vanetix <matmcfarland@gmail.com>
2013-02-03 17:18:59 -0600

    make a mock program and add a mock option, then parse

--- example/example.go
+++ example/example.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/vanetix/commander.go"
 )
 
@@ -9,4 +10,20 @@ func main() {
 		Name: "example",
 		Version: "0.0.1",
 	}
+
+	option := &commander.Option{
+		Name: "help",
+		Tiny: 'h',
+		Verbose: "help",
+		Description: "display usage",
+		Required: true,
+		Callback: func() {
+			prog.Usage()
+		},
+	}
+
+	prog.Add(option)
+
+	fmt.Println(prog)
+	prog.Parse()
 }
